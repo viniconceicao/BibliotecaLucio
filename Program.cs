11b5df9f203f2435e0ba1e0747a77b4e82e297e6
Vinicius de Liz da Conceição <130084519+viniconceicao@users.noreply.github.com>
2025-03-06 21:27:45 -0300

    Alterando o program.cs

--- Program.cs
+++ Program.cs
@@ -5,3 +5,21 @@
         Console.WriteLine("Hello, World!");
     }
 }
+
+// Criar um CRUD do leitor (CADASTRAR, LISTAR, EDITAR e EXCLUIR)
+// Incluir os livros do leitor
+// Editar um livro específico do leitor
+// Remover um livro, por exemplo, que foi perdido
+// Doar um livro para outro leitor
+// Listar todos os leitores e seus respectivos livros
+// Listar um leitor específico e seus respectivos livros
+// Pesquisar por um livro específico, e mostrar os dados do leitor
+
+// Nas classes devem ser utilizados construtores
+// Os dados cadastrados devem ser digitados pelo usuário
+
+// No leitor incluir um atributo para o CPF, garantido assim um identificador único para o leitor
+// - Ao cadastro novo um novo leitor, validar se o CPF informado já não está em uso
+// - Ver método Exists de List
+// A busca pelo leitor deve ser realizado através do seu CPF
+// O usuário deve poder cadastrar quantos leitores ou livros ele desejar
