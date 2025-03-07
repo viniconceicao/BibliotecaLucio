d8820f5aa32cfd97deb004c4c3125a2a2124756d
Vinicius de Liz da Conceição <130084519+viniconceicao@users.noreply.github.com>
2025-03-06 22:19:17 -0300

    Criando CriarLeitor

--- Leitor.cs
+++ Leitor.cs
@@ -4,12 +4,17 @@ public class Leitor
 {
     public string Nome;
     public int Idade;
+    
+    public int CPF;
+
     public List<Livro> LivrosLeitor = new List<Livro>();
 
-    public Leitor(string nome, int idade)
+    public Leitor(string nome, int idade, int cpf)
     {
         Nome = nome;
         Idade = idade;
+        CPF = cpf;
+
     }
 
     public void AdicionarLivro(Livro livro)
@@ -26,5 +31,14 @@ public class Leitor
         leitorDestino.LivrosLeitor.Add(livroDoado);
         this.LivrosLeitor.Remove(livroDoado);
     }
+
+    public static List<Leitor> leitores = new List<Leitor>();
+
+    public void CriarLeitor(string nome, int idade, int cpf)
+    {
+        Leitor leitor = new Leitor(nome, idade, cpf);
+        leitores.Add(leitor);
+    }
+    // Aqui começamos a criar o leitor, que vai ser nosso usuário do programa
 }
 // Depois resolver
