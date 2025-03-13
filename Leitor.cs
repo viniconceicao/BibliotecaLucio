129ff7b9821bb60606e50efc97f587cfabfb6096
Vinicius de Liz da Conceição <130084519+viniconceicao@users.noreply.github.com>
2025-03-12 22:03:09 -0300

    Tirando as funções do Leitor.CS de enviar mensagem

--- Leitor.cs
+++ Leitor.cs
@@ -1,209 +1,130 @@
-namespace Biblioteca;
-
-public class Leitor
+namespace Biblioteca
 {
-    public string Nome { get; set; }
-    public int Idade { get; set; }
-    
-    public string CPF { get; set; } // Alterado para string, pois ele não funcionava, seja com ou sem pontos e traços
-
-    public List<Livro> LivrosLeitor = new List<Livro>();
-
-    public Leitor(string nome, int idade, string cpf) // Alterado para string
+    public class Leitor
     {
-        Nome = nome;
-        Idade = idade;
-        CPF = cpf;
-    }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CPF { get; set; }
+        public List<Livro> LivrosLeitor { get; set; } = new List<Livro>();
 
-    public void AdicionarLivro(Livro livro)
-    {
-        this.LivrosLeitor.Add(livro);
-    }
-
-    public void RemoverLivro(Livro livro)
-    {
-        this.LivrosLeitor.Remove(livro);
-    }
-
-    public void DoarLivro(Livro livroDoado, Leitor leitorDestino){
-        leitorDestino.LivrosLeitor.Add(livroDoado);
-        this.LivrosLeitor.Remove(livroDoado);
-    }
-
-    public static List<Leitor> leitores = new List<Leitor>();
-    // Criando uma nova lista chamada "leitores" para armazenar os leitores
-
-    public static void CriarLeitor(string nome, int idade, string cpf) // Alterado para string
-    {
-        if (leitores.Exists(l => l.CPF == cpf))
+        public Leitor(string nome, int idade, string cpf)
         {
-            Console.WriteLine("Erro: Este CPF já está em uso.");
-            return;
+            Nome = nome;
+            Idade = idade;
+            CPF = cpf;
         }
 
-        var novoLeitor = new Leitor(nome, idade, cpf);
-        leitores.Add(novoLeitor);
-        Console.WriteLine("Leitor cadastrado com sucesso!");
-    }
-    // Aqui começamos a criar o leitor, que vai ser nosso usuário do programa
-
-    public static List<Leitor> ListarLeitores()
-    {
-        return leitores;
-    }
-
-    public void EditarLeitor(string cpf) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        Console.WriteLine("Digite o novo nome do leitor: ");
-        leitor.Nome = Console.ReadLine();
-        // Pedindo o novo nome do leitor
-        Console.WriteLine("Digite a nova idade do leitor: ");
-        leitor.Idade = int.Parse(Console.ReadLine());
-        // Pedindo a nova idade do leitor
-    }
-
-    public void ExcluirLeitor(string cpf) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        leitores.Remove(leitor);
-        // Removendo o leitor da lista de leitores
-    }
-
-    // Incluir os livros do leitor
-    public void ListarLivrosLeitor(string cpf) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        if (leitor != null)
+        public void AdicionarLivro(Livro livro)
         {
-            foreach (Livro livro in leitor.LivrosLeitor)
+            LivrosLeitor.Add(livro);
+        }
+
+        public void RemoverLivro(Livro livro)
+        {
+            LivrosLeitor.Remove(livro);
+        }
+
+        public void DoarLivro(Livro livroDoado, Leitor leitorDestino)
+        {
+            leitorDestino.LivrosLeitor.Add(livroDoado);
+            LivrosLeitor.Remove(livroDoado);
+        }
+
+        public static List<Leitor> leitores = new List<Leitor>();
+
+        public static bool CriarLeitor(string nome, int idade, string cpf)
+        {
+            if (leitores.Exists(l => l.CPF == cpf))
             {
-                Console.WriteLine($"Título: {livro.Titulo}, Escritor: {livro.Escritor}, Editora: {livro.Editora}");
-                // Mostrando o título, escritor e editora do livro
+                return false;
             }
-        }
-        else
-        {
-            Console.WriteLine("Leitor não encontrado.");
-        }
-    }
 
-    // Editar um livro específico do leitor
-    public void EditarLivroLeitor(string cpf, string tituloLivro) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        Livro livro = leitor.LivrosLeitor.Find(livro => livro.Titulo == tituloLivro);
-        // Procurando o livro na lista de livros do leitor pelo título
-
-        if (livro != null)
-        {
-            Console.WriteLine("Digite o novo título do livro: ");
-            livro.Titulo = Console.ReadLine();
-            // Pedindo o novo título do livro
-            Console.WriteLine("Digite o novo escritor do livro: ");
-            livro.Escritor = Console.ReadLine();
-            // Pedindo o novo escritor do livro
-            Console.WriteLine("Digite a nova editora do livro: ");
-            livro.Editora = Console.ReadLine();
-            // Pedindo a nova editora do livro
+            var novoLeitor = new Leitor(nome, idade, cpf);
+            leitores.Add(novoLeitor);
+            return true;
         }
-        else
-        {
-            Console.WriteLine("Livro não encontrado.");
-        }
-    }
 
-    // Remover um livro, por exemplo, que foi perdido
-    public void RemoverLivroLeitor(string cpf, string tituloLivro) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        Livro livro = leitor.LivrosLeitor.Find(livro => livro.Titulo == tituloLivro);
-        // Procurando o livro na lista de livros do leitor pelo título
-
-        if (livro != null)
+        public static List<Leitor> ListarLeitores()
         {
-            leitor.LivrosLeitor.Remove(livro);
-            // Removendo o livro da lista de livros do leitor
+            return leitores;
         }
-        else
-        {
-            Console.WriteLine("Livro não encontrado.");
-        }
-    }
 
-    // Doar um livro para outro leitor
-    public void DoarLivroLeitor(string cpfDoacao, string cpfDestino, string tituloLivro) // Alterado para string
-    {
-        Leitor leitorDoacao = leitores.Find(leitor => leitor.CPF == cpfDoacao);
-        // Procurando o leitor que vai doar o livro na lista de leitores pelo CPF
-        Leitor leitorDestino = leitores.Find(leitor => leitor.CPF == cpfDestino);
-        // Procurando o leitor que vai receber o livro na lista de leitores pelo CPF
-        Livro livro = leitorDoacao.LivrosLeitor.Find(livro => livro.Titulo == tituloLivro);
-        // Procurando o livro na lista de livros do leitor que vai doar o livro pelo título
-
-        if (livro != null)
+        public bool EditarLeitor(string novoNome, int novaIdade)
         {
-            leitorDestino.LivrosLeitor.Add(livro);
-            // Adicionando o livro na lista de livros do leitor que vai receber o livro
-            leitorDoacao.LivrosLeitor.Remove(livro);
-            // Removendo o livro da lista de livros do leitor que vai doar o livro
+            Nome = novoNome;
+            Idade = novaIdade;
+            return true;
         }
-        else
-        {
-            Console.WriteLine("Livro não encontrado.");
-        }
-    }
 
-    // Listar todos os leitores e seus respectivos livros
-    public void ListarLeitoresLivros()
-    {
-        foreach (Leitor leitor in leitores)
+        public static bool ExcluirLeitor(string cpf)
         {
-            Console.WriteLine($"Nome: {leitor.Nome}, Idade: {leitor.Idade}, CPF: {leitor.CPF}");
-            // Mostrando o nome, idade e CPF do leitor
-            foreach (Livro livro in leitor.LivrosLeitor)
+            Leitor leitor = leitores.Find(l => l.CPF == cpf);
+            if (leitor != null)
             {
-                Console.WriteLine($"Título: {livro.Titulo}, Escritor: {livro.Escritor}, Editora: {livro.Editora}");
-                // Mostrando o título, escritor e editora do livro
+                leitores.Remove(leitor);
+                return true;
             }
+            return false;
         }
-    }
 
-    // Listar um leitor específico e seus respectivos livros
-    public void ListarLeitorLivros(string cpf) // Alterado para string
-    {
-        Leitor leitor = leitores.Find(leitor => leitor.CPF == cpf);
-        // Procurando o leitor na lista de leitores pelo CPF
-        Console.WriteLine($"Nome: {leitor.Nome}, Idade: {leitor.Idade}, CPF: {leitor.CPF}");
-        // Mostrando o nome, idade e CPF do leitor
-        foreach (Livro livro in leitor.LivrosLeitor)
+        public List<Livro> ListarLivrosLeitor()
         {
-            Console.WriteLine($"Título: {livro.Titulo}, Escritor: {livro.Escritor}, Editora: {livro.Editora}");
-            // Mostrando o título, escritor e editora do livro
+            return LivrosLeitor;
         }
-    }
 
-    // Pesquisar por um livro específico, e mostrar os dados do leitor
-    public void PesquisarLivroLeitor(string tituloLivro)
-    {
-        foreach (Leitor leitor in leitores)
+        public bool EditarLivroLeitor(string tituloLivro, string novoTitulo, string novoEscritor, string novaEditora)
         {
-            Livro livro = leitor.LivrosLeitor.Find(livro => livro.Titulo == tituloLivro);
-            // Procurando o livro na lista de livros do leitor pelo título
-
+            Livro livro = LivrosLeitor.Find(l => l.Titulo == tituloLivro);
             if (livro != null)
             {
-                Console.WriteLine($"Nome: {leitor.Nome}, Idade: {leitor.Idade}, CPF: {leitor.CPF}");
-                // Mostrando o nome, idade e CPF do leitor
-                Console.WriteLine($"Título: {livro.Titulo}, Escritor: {livro.Escritor}, Editora: {livro.Editora}");
-                // Mostrando o título, escritor e editora do livro
+                livro.Titulo = novoTitulo;
+                livro.Escritor = novoEscritor;
+                livro.Editora = novaEditora;
+                return true;
             }
+            return false;
+        }
+
+        public bool RemoverLivroLeitor(string tituloLivro)
+        {
+            Livro livro = LivrosLeitor.Find(l => l.Titulo == tituloLivro);
+            if (livro != null)
+            {
+                LivrosLeitor.Remove(livro);
+                return true;
+            }
+            return false;
+        }
+
+        public bool DoarLivroLeitor(string cpfDestino, string tituloLivro)
+        {
+            Leitor leitorDestino = leitores.Find(l => l.CPF == cpfDestino);
+            Livro livro = LivrosLeitor.Find(l => l.Titulo == tituloLivro);
+            if (leitorDestino != null && livro != null)
+            {
+                leitorDestino.LivrosLeitor.Add(livro);
+                LivrosLeitor.Remove(livro);
+                return true;
+            }
+            return false;
+        }
+
+        public static Leitor ListarLeitorLivros(string cpf)
+        {
+            return leitores.Find(l => l.CPF == cpf);
+        }
+
+        public static Leitor PesquisarLivroLeitor(string tituloLivro)
+        {
+            foreach (Leitor leitor in leitores)
+            {
+                Livro livro = leitor.LivrosLeitor.Find(l => l.Titulo == tituloLivro);
+                if (livro != null)
+                {
+                    return leitor;
+                }
+            }
+            return null;
         }
     }
 }
